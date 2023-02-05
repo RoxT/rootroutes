8f1ec93f2f433ee3f1dd7a22960fe5df6228e400
Roxanne T <roxanne.e.taylor@gmail.com>
2023-02-05 10:43:49 -0800

    camera

--- RootRoutes/scripts/RootTest.gd
+++ RootRoutes/scripts/RootTest.gd
@@ -3,7 +3,8 @@ extends Node2D
 var FruitingBody = preload("res://scenes/FruitingBody.tscn")
 var rng = RandomNumberGenerator.new()
 onready var camera = $Camera2D
-const CAMERA_SPEED = 100
+const CAMERA_SPEED = 500
+const ZOOM_SPEED = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,6 +28,12 @@ func _physics_process(delta):
 		camera.position.y -= CAMERA_SPEED * delta
 	elif Input.is_action_pressed("ui_down"):
 		camera.position.y += CAMERA_SPEED * delta
+	if Input.is_action_pressed("ui_in"):
+		var desired = camera.zoom - Vector2(ZOOM_SPEED * delta, ZOOM_SPEED * delta)
+		camera.zoom = Vector2(clamp(desired.x, 0.3, 2.0), clamp(desired.x, 0.3, 2.0))
+	elif Input.is_action_pressed("ui_out"):
+		var desired = camera.zoom + Vector2(ZOOM_SPEED * delta, ZOOM_SPEED * delta)
+		camera.zoom = Vector2(clamp(desired.x, 0.3, 2.0), clamp(desired.x, 0.3, 2.0))
 
 func _on_restart():
 	$YouDied/Label.text = "re-spawning...."
